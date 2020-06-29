9879e7da0592830350490d49b138e2eed7f7ee08
Valentina Gorshkova <anitnelav01@yandex.ru>
2020-06-30 01:53:56 +0300

    error correction 3 week

--- js/main.js
+++ js/main.js
@@ -4,14 +4,29 @@ document.addEventListener('DOMContentLoaded', function(){
   let overley = document.querySelector(".overlay");
   let close_overlay = document.querySelector(".overlay__close");
 
+  let menu_links = document.querySelectorAll(".menu__link");
+
+  menu_links.forEach(function(x,i){
+    x.addEventListener("click", function (e) {
+      hideOverlay(overley);
+    });
+  })
+
 
   burger.addEventListener("click", function (e) {
+    e.preventDefault();
     overley.classList.add('overlay--active');
   });
   close_overlay.addEventListener("click", function (e) {
-    overley.classList.remove('overlay--active');
-
+    hideOverlay(overley);
   });
+
+
+  function hideOverlay(overley){
+    if(overley.classList.contains('overlay--active')){
+      overley.classList.remove('overlay--active');
+    }
+  }
 
 
 
