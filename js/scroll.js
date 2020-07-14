6908c5aaba66577ebc2ee3d4909047ed820062f3
Valentina Gorshkova <anitnelav01@yandex.ru>
2020-07-14 04:18:29 +0300

    fix mobile zoom

--- js/scroll.js
+++ js/scroll.js
@@ -126,3 +126,17 @@ $("[data-scroll-to]").click(e => {
 
   performTransition(reqSection.index());
 });
+
+//////https://github.com/mattbryson/TouchSwipe-Jquery-Plugin
+
+$("body").swipe({ 
+  swipe: function(event, direction) {
+    const scroller = viewportcroller();
+    let scrollDirection = "";
+    
+    if (direction ==="up") scrollDirection = "next";
+    if (direction ==="down") scrollDirection = "prev";
+
+  scroller[scrollDirection]();
+  },
+});
